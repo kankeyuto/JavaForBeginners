14fb9d12721edd92c5399a4837a2521a5d512e91
MYNOTEPC\kyuto <kankeyuto11.10@gmail.com>
2025-05-26 10:50:50 +0900

    setColorの追加

---
+++ src/RectTest.java
@@ -0,0 +1,37 @@
+
+public class RectTest extends MyFrame{
+	
+	public void run() {
+//		setColor(255,0,0);
+//		fillRect(30,40,50,100);
+//		setColor(0,255,0);
+//		fillRect(130,40,50,100);
+//		setColor(0,0,255);
+//		fillRect(30,200,150,10);
+//		黄色 255,255,0
+//		紫 128,0,128
+//		水色 0,255,255
+//		黒 0,0,0
+//		灰色 84,84,87
+//		確認問題3.3
+		setColor(255,255,0);
+		fillRect(0,0,100,100);
+		
+		setColor(128,0,128);
+		fillRect(0,100,100,100);
+		
+		setColor(0,255,255);
+		fillRect(0,200,100,100);
+		
+		setColor(0,0,0);
+		fillRect(100,100,100,100);
+		
+		setColor(84,84,87);
+		fillRect(100,200,100,100);
+		
+		setColor(255,255,255);
+		fillRect(100,300,100,100);
+	}
+
+
+}
--- src/Start.java
+++ src/Start.java
@@ -3,6 +3,10 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+//		System.out.println("Hello World");
+		MyFrame frame1 = new RectTest();
+		MyFrame frame2 = new OvalTest();
 
 	}
 
